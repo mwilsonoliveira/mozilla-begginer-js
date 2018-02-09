bc170dde3ea1e622368a31dd356fd22a7e7866e2
Marcos Wilson <mwilson.oliveira@gmail.com>
2018-02-09 08:02:50 -0200

    Adição da página Javascript List

---
+++ scripts/javascript-list.js
@@ -0,0 +1,22 @@
+var contadorNovoItem = 1
+var minhaLista = document.querySelector("#minha-lista")
+//var minhaLista = document.getElementById("minha-lista")
+var meuBotao = document.getElementById("meu-botao")
+var meuCabecalho = document.getElementById("meu-cabecalho")
+var itensLista = document.querySelectorAll("#minha-lista li")
+//var itensLista = document.getElementById("minha-lista").getElementsByTagName("li")
+
+minhaLista.addEventListener("click", ativaItem)
+
+function ativaItem(e){
+  if(e.target.nodeName == "LI"){
+    meuCabecalho.innerHTML = e.target.innerHTML
+  }
+}
+
+meuBotao.addEventListener("click", criaNovoItem)
+
+function criaNovoItem(){
+  minhaLista.innerHTML += "<li>Novo Item " + contadorNovoItem + "</li>"
+  contadorNovoItem++
+}
