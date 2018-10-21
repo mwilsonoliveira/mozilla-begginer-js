68af6b2610f44a7fa3706974039fc153fb9fd78f
Marcos Wilson <mwilson.oliveira@gmail.com>
2018-10-20 22:17:22 -0300

    Adição de vários exerÂcícios!

--- scripts/rocketseat-ajax.js
+++ scripts/rocketseat-ajax.js
@@ -1,6 +1,9 @@
+var ajax = document.getElementById('ajax');
+var conteudo = document.createElement('p');
+
 /* Isso aqui é um ajax raiz. Ele tá abrindo um XML request
-e jogando em uma variável qualquer chamada xhr.
-*/
+e jogando em uma variável qualquer chamada xhr. */
+
 var xhr = new XMLHttpRequest();
 
 //Pronto, agora já podemos acessar as propriedades da função,
@@ -10,12 +13,14 @@ xhr.open('GET', 'https://api.github.com/users/mwilsonoliveira');
 //foi um null pra ele.
 xhr.send(null);
 
-/*Agora na propriedade "onreadystatechange" jogamos uma função.
+/* Agora na propriedade "onreadystatechange" jogamos uma função.
 Essa função verifica se o estado é '4' ou seja, se está ok,
 e aí então retorna em forma de JSON, o texto que veio desse 
 request */
 xhr.onreadystatechange = function() {
   if(xhr.readyState === 4) {
     console.log(JSON.parse(xhr.responseText));
+    conteudo.textContent = JSON.stringify(xhr.responseText);
+    ajax.appendChild(conteudo);
   }
 }
