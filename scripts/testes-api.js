6a603a64cb9847f6a735af4c514e443e7dfeed9c
Marcos Wilson de Oliveira <mwilson.oliveira@gmail.com>
2020-04-03 18:07:07 -0300

    busca cep api

--- scripts/testes-api.js
+++ scripts/testes-api.js
@@ -8,9 +8,63 @@ axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/municipios/431870
         estado.textContent = response.data.microrregiao.mesorregiao.UF.nome;
         ajax.appendChild(cidade);
         ajax.appendChild(estado);
-        console.log(response.data);
-        console.log(response.data.microrregiao.mesorregiao.UF.nome);
     })
     .catch(function(error){
-        console.warn(error);
+        console.log(error)
     });
+
+function searchCep () {
+    // Inicia o preloader
+    startPreloader();
+
+    // Recupera o value do input cep
+    let cep = document.getElementById('cep').value
+
+    // Inicia requisição AJAX com o axios
+    axios.get(`http://api.postmon.com.br/v1/cep/${cep}`, {})
+            .then(response => {
+                showResults(response.data)
+            })
+            .catch(error => {
+                // Mostra a div com o erro
+                document.getElementById('error').style.display = 'block'
+        
+                // Mostra a mensagem
+                document.getElementById('error').innerHTML = 'Erro inesperado'
+            })
+            .finally(() => endPreloader ())
+
+        event.preventDefault()
+}
+
+function showResults (address) {
+    // Mostra a div com o resultado
+    document.getElementById('results').style.display = "block"
+ 
+    // Mostra os resultados:
+    document.getElementById('results').innerHTML = `
+        <p><b>Bairro: </b> ${address.bairro} </p>
+        <p><b>Cidade: </b> ${address.cidade} </p>
+        <p><b>Logradouro: </b> ${address.logradouro} </p>
+        <p><b>Cep: </b> ${address.cep} </p>
+    `
+}
+
+function startPreloader () {
+    // Ocultar a div de erro
+    document.getElementById('error').style.display = 'none'
+
+    // Exibe a div de preloader
+    document.getElementById('preloader').style.display = 'block'
+    
+    // Limpa os dados do resultado:
+    document.getElementById('results').innerHTML = ''
+ 
+    // Oculta a div com o resultado
+    document.getElementById('results').style.display = 'none'
+}
+ 
+function endPreloader () {
+    // Oculta a div de preloader
+    document.getElementById('preloader').style.display = 'none'
+}
