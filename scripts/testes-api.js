78791eed1e7cfa0b1acb2c7221371958b542f3dc
mwilsonoliveira <mwilson.oliveira@gmail.com>
2020-04-12 21:42:49 -0300

    changing testes-api.js

--- scripts/testes-api.js
+++ scripts/testes-api.js
@@ -10,7 +10,7 @@ axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/municipios/431870
         ajax.appendChild(estado);
     })
     .catch(function(error){
-        console.log(error)
+        console.log(error);
     });
 
 function searchCep () {
@@ -18,19 +18,19 @@ function searchCep () {
     startPreloader();
 
     // Recupera o value do input cep
-    let cep = document.getElementById('cep').value
+    let cep = document.getElementById('cep').value;
 
     // Inicia requisição AJAX com o axios
     axios.get(`http://api.postmon.com.br/v1/cep/${cep}`, {})
             .then(response => {
-                showResults(response.data)
+                showResults(response.data);
             })
             .catch(error => {
                 // Mostra a div com o erro
-                document.getElementById('error').style.display = 'block'
+                document.getElementById('error').style.display = 'block';
         
                 // Mostra a mensagem
-                document.getElementById('error').innerHTML = 'Erro inesperado'
+                document.getElementById('error').innerHTML = 'Erro inesperado';
             })
             .finally(() => endPreloader ())
 
@@ -39,7 +39,7 @@ function searchCep () {
 
 function showResults (address) {
     // Mostra a div com o resultado
-    document.getElementById('results').style.display = "block"
+    document.getElementById('results').style.display = "block";
  
     // Mostra os resultados:
     document.getElementById('results').innerHTML = `
@@ -47,24 +47,25 @@ function showResults (address) {
         <p><b>Cidade: </b> ${address.cidade} </p>
         <p><b>Logradouro: </b> ${address.logradouro} </p>
         <p><b>Cep: </b> ${address.cep} </p>
-    `
+    `;
 }
 
 function startPreloader () {
     // Ocultar a div de erro
-    document.getElementById('error').style.display = 'none'
+    document.getElementById('error').style.display = 'none';
 
     // Exibe a div de preloader
-    document.getElementById('preloader').style.display = 'block'
+    document.getElementById('preloader').style.display = 'block';
     
     // Limpa os dados do resultado:
-    document.getElementById('results').innerHTML = ''
+    document.getElementById('results').innerHTML = '';
  
     // Oculta a div com o resultado
-    document.getElementById('results').style.display = 'none'
+    document.getElementById('results').style.display = 'none';
 }
  
 function endPreloader () {
     // Oculta a div de preloader
-    document.getElementById('preloader').style.display = 'none'
+    document.getElementById('preloader').style.display = 'none';
 }
+
